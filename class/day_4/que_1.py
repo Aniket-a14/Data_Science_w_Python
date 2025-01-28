2c31d6c3cacdec74f9caacfd876cf06d68d26e1c
Aniket Saha <aniketsahaworkspace@gmail.com>
2025-01-28 10:48:02 +0530

    new day new ques

--- class/day_4/que_1.py
+++ class/day_4/que_1.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 a = np.array([1,2,3,4])
-print(a)
+print("Array is:\n",a)
 
 b= np.array([[1,2,3],[4,5,6]])
-print(b)
+print("Array is:\n",b)
 
-c= np.array([[1,2,3],[4,5,6],[7,8,9]])
-print(c)
+c= np.array([[[1,2,3],[9,3,7]],[[4,5,6],[7,8,9]]])
+print("Array is:\n",c)

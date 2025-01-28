567e79649e459fbc2b0a9ca2a078264023b40cb1
Aniket Saha <aniketsahaworkspace@gmail.com>
2025-01-28 11:38:30 +0530

    Add new numpy array operations and create que_2.py for array reshaping

--- class/day_4/que_1.py
+++ class/day_4/que_1.py
@@ -8,3 +8,14 @@ print("Array is:\n",b)
 
 c= np.array([[[1,2,3],[9,3,7]],[[4,5,6],[7,8,9]]])
 print("Array is:\n",c)
+
+d= np.eye(2,3)
+print("Array is:\n",d)
+
+e= np.full([2,3],10)
+print("Array is:\n",e)
+
+print("Cummulative sum column wise:\n",b.cumsum(axis=0))
+print("Cummulative sum row wise:\n",b.cumsum(axis=1))
+print("Flatten function:\n",b.flatten())
+print("Transpose function:\n",b.transpose())
---
+++ class/day_4/que_2.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+l = [i for i in range(1, 11)]
+a = np.array(l).reshape(2, 5)
+print("Array is:\n", a)

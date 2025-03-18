a03747f64652516963805a56e4bebcd304ee134e
Aniket Saha <aniketsahaworkspace@gmail.com>
2025-03-18 10:33:53 +0530

    new day new questions

---
+++ class/day_12/que_1.py
@@ -0,0 +1,21 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+categories = ['Furniture', 'Technology', 'Office Supplies']
+profit = [4390, 6720, 6010]
+sales = [10000, 15000, 12000]
+
+sns.set_theme(style="whitegrid")
+plt.figure(figsize=(8,5))
+sns.lineplot(x=categories, y=sales, label='Sales', marker='o')
+plt.title('Sales Trend by Category')
+plt.xlabel('Category')
+plt.show()
+
+plt.figure(figsize=(8,5))
+barplot = sns.barplot(x=categories, y=profit, palette='viridis', errorbar=None)
+plt.title('Profit by Category')
+for i,value in enumerate(profit):
+    barplot.text(i, value + 100, f'[value:.2f]', ha = 'center')
+    
+plt.show()
